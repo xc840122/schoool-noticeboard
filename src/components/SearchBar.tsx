ac7ead4157d8612bee7af4472095c2c5aa042fca
Chi Xu <xuchi007@hotmail.com>
2025-02-25 13:57:25 +1300

    update search bar

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -4,8 +4,8 @@ import { Input } from "@/components/ui/input"
 const SearchBar = async () => {
   return (
     <div className="flex w-full max-w-sm items-center space-x-2">
-      <Input type="email" placeholder="Email" />
-      <Button type="submit">Subscribe</Button>
+      <Input type="email" placeholder="Please input..." />
+      <Button type="submit">Search</Button>
     </div>
   )
 }
