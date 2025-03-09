740c8e7481bd0784f7af4d8dc72fbf166b51b165
Chi Xu <xuchi007@hotmail.com>
2025-03-09 13:39:34 +1300

    config next.config.js

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
+  dir: './src',
 };
 
 export default nextConfig;
