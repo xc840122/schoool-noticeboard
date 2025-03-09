5b63bf15281970e4da018c5be78bc1358251d9c0
Chi Xu <xuchi007@hotmail.com>
2025-03-09 13:50:58 +1300

    upgrade react-day-picker to solve conflict

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
-  dir: './src',
 };
 
 export default nextConfig;
